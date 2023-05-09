a5134e5e9082bf5df2c6c2f3e92968efea0ac5f1
hwhs-2022-sci <117999747+hwhs-2022-sci@users.noreply.github.com>
2023-05-10 06:51:42 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,47 +3,128 @@ var canvas = new fabric.Canvas("main")
 canvas.setHeight(350);
 canvas.setWidth(900);
 
-var bgImg = new fabric.Image(document.getElementById("background"), {
-  name: `底圖`,
-  top: 0,
-  left: 0,
-})
-
-canvas.setBackgroundImage(bgImg, function () {
-  let img = canvas.backgroundImage;
-  img.originX = 'left';
-  img.originY = 'top';
+canvas.setBackgroundImage("/img/底圖.png", function () {
   canvas.renderAll();
 });
 
-const imgset = document.getElementById("imgset")
-let movingImage
-let imgDragOffset = {
-  offsetX: 0,
-  offsetY: 0,
-}
-function saveImg(e) {
-  if (e.target.tagName.toLowerCase() === 'img') {
-    // 這邊先計算點擊圖片的何處，等滑鼠放開後要重新計算起始座標
-    imgDragOffset.offsetX = e.clientX - e.target.offsetLeft
-    imgDragOffset.offsetY = e.clientY - e.target.offsetTop
-    movingImage = e.target
+function findSelection(name) {
+  try {
+    return document.querySelector(`[name="${name}"]:checked`).value
+  } catch(e){
+    return null
   }
 }
-imgset.addEventListener('click', saveImg)
-function dropImg(e) {
 
-  const { offsetX, offsetY } = e.e
-  console.log(movingImage)
-  const image = new fabric.Image(movingImage, {
-    width: movingImage.naturalWidth,
-    height: movingImage.naturalHeight,
+$("#joinBtn").on("click", function () {
 
+  console.log(findSelection("flexRadioDefault"))
+  var imgPath =document.querySelector("#"+findSelection("flexRadioDefault"))
+  var img = 
+  new fabric.Image(imgPath, {
+    name: ``,
+    top: 50,
+    left: 50,
 
-    top: offsetY - imgDragOffset.offsetY, // 計算起始位置
-    left: offsetX - imgDragOffset.offsetX
   })
-  canvas.add(image)
-}
+  canvas.add(img)
+  canvas.renderAll()
+})
 
-canvas.on('drop', dropImg)
+
+
+
+
+fabric.Canvas.prototype.customiseControls({
+  tl: {
+    action: {
+      'rotateByDegrees': -90
+    },
+    cursor:"pointer"
+   
+  },
+  tr: {
+    action: 'rotate',
+    action: {
+      'rotateByDegrees': 90
+    },
+    
+    cursor:"pointer"
+   
+  },
+  bl: {
+    action: 'remove',
+  
+    cursor:"pointer"
+
+  },
+  br: {
+    action: 'moveUp',
+
+  },
+  mb: {
+    action: 'moveDown',
+
+  },
+  mt: {
+    action: {
+      'rotateByDegrees': 90
+    }
+  },
+  mr: {
+    action: function (e, target) {
+      target.set({
+        left: 200
+      });
+      canvas.renderAll();
+    }
+  },
+  // only is hasRotatingPoint is not set to false
+  mtr: {
+    action: 'rotate',
+
+  },
+}, function () {
+  canvas.renderAll();
+});
+fabric.Object.prototype.customiseCornerIcons({
+  settings: {
+      borderColor: '#0094dd',
+      cornerSize: 15,
+      cornerShape: 'rect',
+      cornerBackgroundColor: 'white',
+  },
+  tl: {
+    icon:"/img/arrow-counterclockwise.svg"
+  },
+  tr: {
+    icon:"/img/arrow-clockwise.svg"
+  },
+  ml: {
+      
+  },
+  mr: {
+     
+  },
+  bl:{
+    icon:"/img/trash3.svg"
+
+  },
+  // only is hasRotatingPoint is not set to false
+  mtr: {
+      
+  },
+}, function() {
+  canvas.renderAll();
+});
+
+fabric.Object.prototype.setControlsVisibility({
+  bl: true, // 左下
+  br: false, // 右下
+  mb: false, // 下中
+  ml: false, // 中左
+  mr: false, // 中右
+  mt: false, // 上中
+  tl: true, // 上左
+  tr: true, // 上右
+  mtr: false // 旋轉控制鍵
+})
